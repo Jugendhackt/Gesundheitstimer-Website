424f3a4178bd4e03688361e61eedb025d18ca9c9
2mal3 <56305732+2mal3@users.noreply.github.com>
2023-06-18 09:27:25 +0200

    optimierung für pycharm

--- gesundheitstimer/__main__.py
+++
@@ -1,4 +0,0 @@
-from gesundheitstimer.main import main
-
-if __name__ == "__main__":
-    main()
--- gesundheitstimer/main.py
+++ gesundheitstimer/main.py
@@ -115,3 +115,7 @@ def main():
         db.Setting.create(key="alarm", value=0)
 
     app.run(host="0.0.0.0", port=5000)
+
+
+if __name__ == "__main__":
+    main()
